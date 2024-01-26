fb03d2e96a70c37dc5fd375becdafaeade3a85a5
piperbates <piperbates42@gmail.com>
2024-01-26 15:04:26 +0000

    hero tablet view

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+"use client"; // This is a client component 👈🏽
+
+import React, { useState } from "react";
 import styles from "./hero.module.css";
 import Image from "next/image";
 import trustScore from "@/assets/images/trustpilot.png"
 import BlueButton from "../blueButton";
+  
+const formTypes = {
+    RETURN: "return",
+    ONEWAY: "one way",
+    MULTICITY: "multicity"
+}
 
 const FormBox = ({boxTitle, info, subText}: {boxTitle: string, info: string, subText?: string}) => 
     <div className={styles.formBox}>
@@ -13,12 +21,64 @@ const FormBox = ({boxTitle, info, subText}: {boxTitle: string, info: string, sub
         <p>{info}</p>
     </div>
 
-const FormButton = ({selected, title}: {selected?: boolean, title: string}) =>
-    <button className={selected ? styles.selectedFormType : styles.unselectedFormType}>
+const FormButton = ({selected, title, onClick}: {selected?: boolean, title: string, onClick: ()=>void}) =>
+    <button className={selected ? styles.selectedFormType : styles.unselectedFormType} onClick={()=>onClick()}>
         <p>{title}</p>
     </button>
 
-const Hero = () => (
+
+const FormContent = ({formType}: {formType?: string}) => {
+    switch(formType) {
+        case "return":
+            return <div className={styles.bookingFormDetails}>
+                <div className={styles.bookingFormDestinations}>
+                    <FormBox boxTitle="Where from?" info="London (LON)" />
+                    <FormBox boxTitle="Where to?" info="Sydney (SYD)" />
+                </div>
+                <div className={styles.bookingFormExtraDetails}>
+                    <FormBox boxTitle="Dates" info="Sat 11 Jun - Sat 25 Jun" subText="14 Nights" />
+                    <FormBox boxTitle="Passengers" info="2 Adults, Any class" />         
+                </div>
+            </div>
+        case "one way":
+            return <div className={styles.bookingFormDetails}>
+                <FormBox boxTitle="Where from?" info="London (LON)" />
+                <FormBox boxTitle="Where to?" info="Sydney (SYD)" />
+                <FormBox boxTitle="Dates" info="Sat 11 Jun" />
+                <FormBox boxTitle="Passengers" info="2 Adults, Any class" />         
+            </div>
+        case "multicity":
+            return <div className={styles.bookingFormDetails}>
+                <FormBox boxTitle="Passengers" info="2 Adults, Any class" />         
+                <FormBox boxTitle="Starting from?" info="London (LON)" />
+                <FormBox boxTitle="First destination" info="Sydney (SYD)" />
+                <FormBox boxTitle="Next destination" info="Sydney (SYD)" />
+
+                <button className={styles.bookingFormAddCity}>Add new city</button>
+                <FormBox boxTitle="Dates" info="Sat 11 Jun" />
+            </div>
+    }
+}
+
+
+const Hero = () => {
+    const [formType, setFormType] = useState(formTypes.RETURN);
+
+    switch(formType) {
+        case formTypes.RETURN:
+            <p>Return</p>
+            break;
+
+        case formTypes.ONEWAY:
+            <p>One Way</p>
+            break;
+        
+        case formTypes.MULTICITY:
+            <p>Multi-city</p>
+            break;
+
+    }
+    return (
     <div className={styles.hero}>
         <div className={styles.heroContent}>
         <h1>Search and book flights to Australia</h1>
@@ -38,18 +98,19 @@ const Hero = () => (
 
         <div className={styles.bookingForm}> 
             <div className={styles.bookingFormHeader}>
-                <FormButton selected title="Return"/>
-                <FormButton title="One way"/>
-                <FormButton title="Multi-city"/>
 
+            <div className={styles.mainButtons}>
+                <FormButton selected title="Return" onClick={()=>{setFormType(formTypes.RETURN)}} />
+                <FormButton title="One way" onClick={()=>{setFormType(formTypes.ONEWAY)}}/>
+                <FormButton title="Multi-city" onClick={()=>{setFormType(formTypes.MULTICITY)}}/>
+        </div>
+
+                <div className={styles.directFlightsToggle}>
+                    <label htmlFor="directFlight">Direct flights only</label><input type="checkbox" name="directFlight" />
+                </div>
             </div>
 
-            <div className={styles.bookingFormDetails}>
-                <FormBox boxTitle="Where from?" info="London (LON)" />
-                <FormBox boxTitle="Where to?" info="Sydney (SYD)" />
-                <FormBox boxTitle="Dates" info="Sat 11 Jun - Sat 25 Jun" subText="14 Nights" />
-                <FormBox boxTitle="Passangers" info="2 Adults, Any class" />         
-            </div>
+            <FormContent formType={formType} />
 
             <BlueButton>
                 <span>Search flights</span>
@@ -59,6 +120,6 @@ const Hero = () => (
 
         
     </div>
-)
+)}
 
 export default Hero;
