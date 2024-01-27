23aeaddbe959f30d106d579155a906d9a280ddb4
piperbates <piperbates42@gmail.com>
2024-01-27 01:11:43 +0000

    desktop view progress

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,6 +9,15 @@ const Header = () => (
         <div className={styles.aaLogoWrapper}>
             <Image src={alternativeAirlines.src} fill alt={'logo'} />
         </div>
+        <div className={styles.navigationList}>
+            <ul>
+                <li className={styles.moreNav}>Book flights</li>
+                <li className={styles.moreNav}>Information</li>
+                <li className={styles.moreNav}>Help</li>
+                <li>Travel Agents</li>
+                <li>Manage booking</li>
+            </ul>
+        </div>
 
         <div className={styles.hamburger}>
             <Image src={logo.src} fill alt="menu" />
